b7eba533faa09578e9e7ae07bf1523af52219986
sam woolf <sam.woolf@gmail.com>
2017-02-10 09:41:16 +0000

    made loader look better

--- StopAndSearch/Loader.swift
+++ StopAndSearch/Loader.swift
@@ -11,35 +11,37 @@ import UIKit
 class Loader: UIView {
 
 
-  var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.white)
+  var activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
   var strLabel : UILabel
   
   init(message: String) {
    
   
-  strLabel = UILabel(frame: CGRect(x: 50, y: 0, width: 200, height: 50))
-  super.init(frame: CGRect(x:0, y:0, width: 180, height: 50))
+  strLabel = UILabel(frame: CGRect(x: 50, y: 0, width: 250, height: 50))
+  super.init(frame: CGRect(x:0, y:0, width: 250, height: 50))
   self.layer.cornerRadius = 15
-  self.backgroundColor = UIColor.darkGray.withAlphaComponent(0.8)
+  self.backgroundColor = UIColor.lightGray.withAlphaComponent(0.3)
+  let blurEffect = UIBlurEffect(style: UIBlurEffectStyle.light)
+  let blurEffectView = UIVisualEffectView(effect: blurEffect)
+  blurEffectView.layer.cornerRadius = 15
+  blurEffectView.clipsToBounds = true
+  blurEffectView.frame = self.bounds
+  self.addSubview(blurEffectView)
+
+    
+    
+    
   strLabel.text = message
-  strLabel.textColor = UIColor.white
+  strLabel.textColor = UIColor.darkGray
   addSubview(strLabel)
   activityIndicator.frame = CGRect(x: frame.origin.x + 5  , y: frame.midY-25, width: 50, height: 50)
   activityIndicator.startAnimating()
   addSubview(activityIndicator)
- // applyPlainShadow(view: self)
+
   }
   
   required init?(coder aDecoder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  func applyPlainShadow(view: UIView) {
-    let l = view.layer
-    
-    l.shadowColor = UIColor.black.cgColor
-    l.shadowOffset = CGSize(width: 2, height: 2)
-    l.shadowOpacity = 0.2
-    l.shadowRadius = 3
-  }
 }
